49511e5cf1a234309da083978c2a6cd8f9c4e417
JRobertoCarrera <42445358+JRobertoCarrera@users.noreply.github.com>
2019-06-10 13:24:12 -0500

    VR HANDS PREFAB
    El prefab esta dentro de la carpeta CartoonMotion Studio :)

---
+++ Assets/Scripts/Disparar.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disparar : MonoBehaviour
+{
+    public Animator anime;
+    public GameObject manopla;
+   // private SteamVR_TrackedController device;
+
+    void Start()
+    {
+      //  device = GetComponent<SteamVR_TrackedController>();
+       //device.TriggerClicked += trigger;
+    }
+
+    void Update()
+    { 
+        if (Input.GetKey("space"))
+        {
+            manopla.GetComponent<Animator>().Play("trigger3");
+        }
+        else
+        {
+        manopla.GetComponent<Animator>().Play("New State");
+        }
+    }
+
+   /* void trigger (object sender, ClickedEventArgs e)
+    {
+        Debug.Log("Disparo");
+        SteamVR_Controller.Input((int)device.index).TriggerHapticPulse(500);
+    }*/
+}
